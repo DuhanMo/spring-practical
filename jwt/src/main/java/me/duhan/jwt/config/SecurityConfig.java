a89b3f0f9e1891b6851edc34618350b29de5a3fc
moduhan <devduhanmo@gmail.com>
2022-01-18 10:31:02 +0900

    feat: security 기본설정

---
+++ jwt/src/main/java/me/duhan/jwt/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package me.duhan.jwt.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .requestMatchers(
+                        PathRequest.toH2Console(),
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+    }
+}
